5cbc389d8feaaf0a97b4b1af8e03c0c371dc2a6f
Reny Reyes <renyreyes8@hotmail.com>
2023-03-15 15:08:09 -0400

    Created the bubble sort structure sort functionality

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-function logSomething(): void {
-  console.log('something');
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    // const len = this.collection.length
+    // DESTRUCTED BELOW
+    const { length } = this.collection;
+    // BUBBLE SORT STRUCTURE
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftElement = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftElement;
+        }
+      }
+    }
+    console.log(this.collection);
+  }
 }
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
